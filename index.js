34b5be6ff3db3942a22140079b71d23d72d3c4e8
mkovalow <maxi.kovalow@gmail.com>
2024-10-20 19:57:03 -0300

    Commit12

--- index.js
+++ index.js
@@ -188,7 +188,7 @@ app.post('/v1/ventas', async (req, res) => {
 app.get('/v1/clientes/:id/ventas', async (req, res) => {
     try {
         const id_cliente = parseInt(req.params.id, 10);
-        let compras = await ServicioClientes.getByCliente(id_cliente);
+        let compras = await ServicioVentas.getByCliente(id_cliente);
         res.json(compras);
     } catch (error) {
         console.error(error);
